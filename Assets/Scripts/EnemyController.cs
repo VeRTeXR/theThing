b749e269e6fd24595b17b2c3d37da98a747194f1
VeRTeXR <p5vasut27665@gmail.com>
2017-09-24 16:02:46 +0700

    enemy prefab reworked

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Security.Principal;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyController: MonoBehaviour {
 
 	private int enemyHP;
 	private float speed;
@@ -58,7 +58,8 @@ public class EnemyScript : MonoBehaviour {
 		Debug.Log(gameObject.name + "is engaging :: " + go.gameObject.name);
 		while (_isEngaging)
 		{
-			LookAtPlayer();
+			//LookAtPlayer();
+			MoveToPlayer();
 			yield return new WaitForSeconds(0.3f);
 		}
 		yield return null;
@@ -108,8 +109,8 @@ public class EnemyScript : MonoBehaviour {
 
 	private void MoveToPlayer()
 	{
-		//GetComponent<Rigidbody2D>().velocity = (Player.transform.position - transform.position);
-		//GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed); //movement code
+		GetComponent<Rigidbody2D>().velocity = (Player.transform.position - transform.position);
+		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed); //movement code
 	}
 	
 	void Update ()
@@ -128,96 +129,6 @@ public class EnemyScript : MonoBehaviour {
 			AudioSource.PlayClipAtPoint(explosion,transform.position);
 		}
 	}
-/*	
-	/**
-	 * Pause the mover
-	 * 
-	 #1#
-	 void Pause()
-	 {
-	 	isWaiting = !isWaiting;
-	 }
-
-
-
-	/**
-	 * Move the object towards the selected waypoint
-	 * integrate this movement shit into the normal rigidbody2d code
-	 #1#
-	 private void MoveTowardsWaypoint()
-	 {
-		// Get the moving objects current position
-	 	Vector3 currentPosition = this.transform.position;
-
-		// Get the target waypoints position
-	 	Vector3 targetPosition = currentWaypoint.transform.position;
-
-		// If the moving object isn't that close to the waypoint
-	 	if(Vector3.Distance(currentPosition, targetPosition) > .1f) {
-
-			// Get the direction and normalize
-	 		Vector3 directionOfTravel = targetPosition - currentPosition;
-	 		directionOfTravel.Normalize();
-
-			//scale the movement on each axis by the directionOfTravel vector components
-	 		this.transform.Translate(
-	 			directionOfTravel.x * speed * Time.deltaTime,
-	 			directionOfTravel.y * speed * Time.deltaTime,
-	 			directionOfTravel.z * speed * Time.deltaTime,
-	 			Space.World
-	 			);
-	 	} else {
-
-			// If the waypoint has a pause amount then wait a bit
-	 		if(currentWaypoint.waitSeconds > 0) {
-	 			Pause();
-	 			Invoke("Pause", currentWaypoint.waitSeconds);
-	 		}
-
-			// If the current waypoint has a speed change then change to it
-	 		if(currentWaypoint.speedOut > 0) {
-	 			speedStorage = speed;
-	 			speed = currentWaypoint.speedOut;
-	 		} else if(speedStorage != 0) {
-	 			speed = speedStorage;
-	 			speedStorage = 0;
-	 		}
-
-	 		NextWaypoint();
-	 	}
-	 }
-
-
-
-
-
-	/**
-	 * Work out what the next waypoint is going to be
-	 * 
-	 #1#
-	 private void NextWaypoint()
-	 {
-	 	if(isCircular) {
-
-	 		if(!inReverse) {
-	 			currentIndex = (currentIndex+1 >= wayPoints.Length) ? 0 : currentIndex+1;
-	 		} else {
-	 			currentIndex = (currentIndex == 0) ? wayPoints.Length-1 : currentIndex-1;
-	 		}
-
-	 	} else {
-
-			// If at the start or the end then reverse
-	 		if((!inReverse && currentIndex+1 >= wayPoints.Length) || (inReverse && currentIndex == 0)) {
-	 			inReverse = !inReverse;
-	 		}
-	 		currentIndex = (!inReverse) ? currentIndex+1 : currentIndex-1;
-
-	 	}
-
-	 	currentWaypoint = wayPoints[currentIndex];
-	 }*/
-
 
 	void OnExplode() {
 		if (enemyCount == 1) {
